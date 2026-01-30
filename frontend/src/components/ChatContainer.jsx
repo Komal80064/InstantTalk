328649717e35d62c79011206c2970b684ddba0b8
Komal <panwarkomal2003@gmail.com>
2026-01-30 12:36:37 +0530

    project code

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -34,7 +34,7 @@ const ChatContainer = () => {
   }
 
   return (
-    <div className="flex-1 flex flex-col overflow-auto sm:mb-5">
+    <div className="flex-1 flex flex-col overflow-auto sm:mb-10">
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
